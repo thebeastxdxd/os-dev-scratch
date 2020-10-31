553c6d0b741c3ccb8eaf734c2dc8372c8ddf6a7a
thebeastxdxd <idoba12@gmail.com>
2020-10-31 16:31:37 +0200

    basic boot sector printing to screen

--- boot_sect.s
+++ boot_sect.s
@@ -1,11 +1,26 @@
 ;
-; A simple boot sector program that loops forever
+; A simple boot sector program that prints a message to the screen
+; using a BIOS ISR (interrupt service routine)
 ;
 
-loop:        ; Define a label, "loop", that will allow
-			 ; us to jump back to it, forever.
+mov ah, 0x0e         ; int 10/ah = 0eh -> scrolling teletype BIOS routine
 
-	jmp loop 
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $              ; Jump to the current address (i.e. forever).
+
+;
+; Padding and magic BIOS number.
+;
 
 times 510-($-$$) db 0
 
