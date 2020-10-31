dc643504b08f10abc52ad781a4859594d17a888e
thebeastxdxd <idoba12@gmail.com>
2020-10-31 16:44:15 +0200

    boot sector understanding labels

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@
 
 boot: boot_sect.s
 	nasm boot_sect.s -f bin -o boot_sect.bin
+
+
+run: boot_sect.bin
+	qemu-system-i386 boot_sect.bin
--- boot_sect.s
+++ boot_sect.s
@@ -5,24 +5,32 @@
 
 mov ah, 0x0e         ; int 10/ah = 0eh -> scrolling teletype BIOS routine
 
-mov al, 'H'
+; First attempt
+mov al, the_secret
 int 0x10
-mov al, 'e'
+
+; Second attempt
+mov al, [the_secret]
 int 0x10
-mov al, 'l'
+
+; Third attempt 
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
+
+; Fourth attempt
+mov al, [0x7c1d]
 int 0x10
 
 jmp $              ; Jump to the current address (i.e. forever).
+
+the_secret:
+db "X"
 
 ;
 ; Padding and magic BIOS number.
 ;
 
 times 510-($-$$) db 0
-
-
 dw 0xaa55
